e822f7203f647dd957fac9788c72a52e96991c83
florian.zimmer <florian.zimmer@cosee.biz>
2022-11-09 15:34:59 +0100

    fix: eslint setup/typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'standard-with-typescript'
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   },
-};
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json']
+      }
+    }
+  ]
+}
