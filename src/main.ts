eeaf55094b23abcbf7d2a19ba1c4848ff5fb0a9f
Florian Zimmer <Florian.Zimmer11@web.de>
2022-11-12 11:11:53 +0100

    fix: remove unnecessary axios dependency

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import './style.sass'
 import App from './App.vue'
 import { worker } from './mocks/msw'
-import VueAxios from 'vue-axios'
-import axios from 'axios'
 
 void (worker.start())
 
 const app = createApp(App)
-app.use(VueAxios, axios)
 app.mount('#app')
